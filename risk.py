1a487d8222b74c60ec30ba02163dda90b5199d49
Claudio Caletti <caletti.claudio@gmail.com>
2015-11-21 01:47:04 +0100

    Risk practice

--- risk.py
+++ risk.py
@@ -9,8 +9,13 @@ def run(attack_outcome, defence_outcome):
     import operator
     from collections import Counter
     min_length = min(len(attack_outcome), len(defence_outcome))
-    sorted_attack = sorted(attack_outcome, reverse=True)[:min_length]
-    sorted_defence = sorted(defence_outcome, reverse=True)[:min_length]
+    sorted_attack = sorted(attack_outcome, reverse=True)
+    sorted_defence = sorted(defence_outcome, reverse=True)
+    print "~~~~~~"
+    print "attack", sorted_attack
+    print "defence", sorted_defence
+    sorted_attack = sorted_attack[:min_length]
+    sorted_defence = sorted_defence[:min_length]
     attack_wins = map(operator.gt, sorted_attack, sorted_defence)
     return Counter(attack_wins)
 
@@ -34,12 +39,27 @@ def single_step(attack, defence):
     return float(c)/n
 
 if __name__ == "__main__":
-    for i in range(1,5):
-        print i
-        x = range(1, 20)
-        y = [single_step(j, i) for j in x]
-        plt.plot(x, y, lw = 3, label="defence tanks number:" + str(i))
-    plt.ylabel("attacker win probability")
-    plt.xlabel("attack tanks number")
-    plt.legend(loc = 'center right')
-    plt.show()
+    import sys
+    attack = int(sys.argv[1])
+    defence = int(sys.argv[2])
+    attack_outcome = throw(min(3, attack))
+    defence_outcome = throw(min(3, defence))
+    if len(sys.argv) > 3:
+        if simulation(attack, defence) == 0:
+            print "defence wins"
+        else:
+            print "attack wins"
+    else:
+        result = run(attack_outcome, defence_outcome)
+        print "defence removes", result[True]
+        print "attack removes", result[False]
+
+    #for i in range(1,5):
+    #    print i
+    #    x = range(1, 20)
+    #    y = [single_step(j, i) for j in x]
+    #    plt.plot(x, y, lw = 3, label="defence tanks number:" + str(i))
+    #plt.ylabel("attacker win probability")
+    #plt.xlabel("attack tanks number")
+    #plt.legend(loc = 'center right')
+    #plt.show()
